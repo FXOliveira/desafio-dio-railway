3649a4bcef05472c97ad07dde0c734ccbad8e8c2
FXOliveira <81167481+FXOliveira@users.noreply.github.com>
2024-08-19 19:36:46 -0300

    implementação da entidade usuário e o repository atrelado a este.

---
+++ railway/src/main/java/dio/com/project/railway/model/User.java
@@ -0,0 +1,21 @@
+package dio.com.project.railway.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+}
